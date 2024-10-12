d5b696824291d4f9ae47c0e3bc7c2d36c52b535c
李萌霆 <1264233536@qq.com>
2024-10-12 17:58:31 +0800

    完成39

--- src/main/java/org/example/剑指offer/栈/LCR039柱状图中最大矩形.java
+++ src/main/java/org/example/剑指offer/栈/LCR039柱状图中最大矩形.java
@@ -18,25 +18,29 @@ public class LCR039柱状图中最大矩形 {
         System.out.println(a.largestRectangleArea(new int[]{1}));
     }
 
+    /*单调栈（从栈顶到栈底单调递减） 核心在于求以某根柱子为顶的最大面积，可以转化为找到左边第一根小于该柱子的柱子，找到右边第一根小于该柱子的柱子，两根柱子之内就是最大面积的区域*/
     public int largestRectangleArea(int[] heights) {
         Stack<Integer> stack = new Stack<>();
+        /*需要在数组的首位加上0加上边界，为的是解决heights位单调数组的问题*/
         int[] p = new int[heights.length + 2];
         for (int i = 0; i < heights.length; i++) {
             p[i + 1] = heights[i];
         }
         int max = 0;
         for (int i = 0; i < p.length; i++) {
-            if (stack.isEmpty()||p[i] >= p[stack.peek()])
+            /*栈为空或者栈顶元素小于等于数组元素，则入栈*/
+            if (stack.isEmpty() || p[i] >= p[stack.peek()])
                 stack.push(i);
             else {
+                /*当遍历到的元素大于栈顶元素，则找到了大于栈顶元素的右方的第一根柱子，同时单调递减栈保证了栈顶的下一个元素为栈顶元素左方小于栈顶元素的第一根柱子。*/
                 while (p[i] < p[stack.peek()] && stack.peek() != 0) {
-                    stack.pop();
-                    max = Math.max(max, ((i - stack.peek()) * p[stack.peek()]));
+                    int temp = stack.pop();
+                    /*求以栈顶元素为顶的最大面积*/
+                    max = Math.max(max, ((i - stack.peek() - 1) * p[temp]));
                 }
                 stack.push(i);
             }
         }
-        //todo
         return max;
     }
 }
